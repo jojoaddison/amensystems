03dd816d613f7d4ccedc2c2bc32838aba0146053
Kojo Ampia <kojo.ampia@jojoaddison.net>
2025-07-01 15:23:47 +0200

    changes

---
+++ deploy.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Clean Build
+build(){
+mvn -Pprod clean package -DskipTests
+yarn build
+}
+
+# Deploy to test
+totest(){
+scp target/AmenSupermarketServer.jar jack@dev.gahano.at:/home/jack/amen/.
+rsync -azr target/www/* jack@dev.gahano.at:/home/jack/webroot/amen/dev/.
+}
+
+# Deploy to prod
+
+toprod(){
+scp target/AmenSupermarketServer.jar root@host.gahano.at:/opt/webservices/amen/.
+rsync -azr target/www/* root@host.gahano.at:/var/www/vhosts/amen-supermarket.at/httpdocs/.
+}
+
+
+case "$1" in
+ build)
+	build
+   ;;
+ totest)
+	totest
+   ;;
+ toprod)
+	toprod
+   ;;
+ buildtest)
+	build
+	totest
+	;;
+ buildprod)
+	build
+	toprod
+	;;
+ buildall)
+	build
+	totest
+	toprod
+	;;
+ *)
+   echo "Usage: deploy {biuld|buildall|buildtest|buildprod|totest|toprod}" >&2
+   exit 3
+   ;;
+esac
